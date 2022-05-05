f37a2a2fd1008069deabab69336b7746aa2a1980
josiahwebb <jj.webb520@gmail.com>
2022-05-04 21:30:40 -0600

    styles most of header component, adds fontawesome pkg

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,12 +1,12 @@
 export default function Header() {
     return (
         <div>
-            <img src="" />
-            <h1>Josiah Webb</h1>
-            <h3>Frontend Developer</h3>
-            <p>josiahwebb.com</p>
+            <div className="headshot"></div>
+            <h1 className="name">Josiah Webb</h1>
+            <h3 className="title">Frontend Developer</h3>
+            <a href="" className="website">josiahwebb.com</a>
             <div className="contact-links">
-                <a href=""><img></img>Email</a>
+                <a href=""><FontAwesomeIcon icon="fa-solid fa-envelope" />Email</a>
                 <a href=""><img></img>LinkedIn</a>
             </div>
         </div>
