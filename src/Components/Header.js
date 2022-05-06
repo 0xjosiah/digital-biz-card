39de347a2e43fa27c71d5510ec1d5cc80507e417
josiahwebb <jj.webb520@gmail.com>
2022-05-05 21:09:11 -0600

    adds fontawesome correctly, styles header component

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,14 +1,15 @@
-export default function Header() {
-    return (
-        <div>
-            <div className="headshot"></div>
-            <h1 className="name">Josiah Webb</h1>
-            <h3 className="title">Frontend Developer</h3>
-            <a href="" className="website">josiahwebb.com</a>
-            <div className="contact-links">
-                <a href=""><FontAwesomeIcon icon="fa-solid fa-envelope" />Email</a>
-                <a href=""><img></img>LinkedIn</a>
-            </div>
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { solid, brands } from '@fortawesome/fontawesome-svg-core/import.macro' // <-- import styles to be used
+
+export default () => (
+    <div>
+        <div className="headshot"></div>
+        <h1 className="name">Josiah Webb</h1>
+        <h3 className="title">Frontend Developer</h3>
+        <a href="" className="website">josiahwebb.com</a>
+        <div className="contact-links">
+            <a className="contact-link__email" href=""><FontAwesomeIcon className="contact-links__icon" icon={solid('envelope')} />Email</a>
+            <a className="contact-link__linked" href=""><FontAwesomeIcon className="contact-links__icon" icon={brands('linkedin')} />LinkedIn</a>
         </div>
-    )
-}
+    </div>
+)
