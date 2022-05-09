6cfdde0e0f3ffa81367665203dcb3db10a1f291f
josiahwebb <jj.webb520@gmail.com>
2022-05-09 14:17:30 -0600

    enables footer links

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -4,10 +4,10 @@ import { brands } from '@fortawesome/fontawesome-svg-core/import.macro' // <-- i
 export default function Footer() {
     return (
         <div className='footer'>
-            <a className="social-link" href=""><FontAwesomeIcon className="social-links__icon" icon={brands('twitter')} /></a>
-            <a className="social-link" href=""><FontAwesomeIcon className="social-links__icon" icon={brands('instagram')} /></a>
-            <a className="social-link" href=""><FontAwesomeIcon className="social-links__icon" icon={brands('github')} /></a>
-            <a className="social-link" href=""><FontAwesomeIcon className="social-links__icon" icon={brands('dev')} /></a>
+            <a className="social-link" target="_blank" href="https://www.twitter.com/0xjosiah/"><FontAwesomeIcon className="social-links__icon" icon={brands('twitter')} /></a>
+            <a className="social-link" target="_blank" href="https://www.instagram.com/0xjosiah"><FontAwesomeIcon className="social-links__icon" icon={brands('instagram')} /></a>
+            <a className="social-link" target="_blank" href="https://www.github.com/0xjosiah"><FontAwesomeIcon className="social-links__icon" icon={brands('github')} /></a>
+            <a className="social-link" target="_blank" href="https://www.dev.to/0xjosiah"><FontAwesomeIcon className="social-links__icon" icon={brands('dev')} /></a>
         </div>
     )
 }
