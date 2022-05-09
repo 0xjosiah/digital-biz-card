4a843db2a504b03129e875dd19f4b9ccfa443f40
josiahwebb <jj.webb520@gmail.com>
2022-05-09 14:22:30 -0600

    fixes spelling error

--- src/Components/Interests.js
+++ src/Components/Interests.js
@@ -2,7 +2,7 @@ export default function Interests() {
     return (
         <div className="info">
             <h2 className="info-header">Interests</h2>
-            <p className="info-text">Mountain biking. Skiing. Music of all sorts (except country). Good Books. Finance. IPA enjoyer.</p>
+            <p className="info-text">Mountain biking. Skiing. Music of all sorts (except country). Good books. Finance. IPA enjoyer.</p>
         </div>
     )
 }
