0f43e7cd49e93775bf62683595a729a854da5fa7
josiahwebb <jj.webb520@gmail.com>
2022-05-09 13:10:54 -0600

    styles footer component

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,10 +1,13 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { brands } from '@fortawesome/fontawesome-svg-core/import.macro' // <-- import styles to be used
+
 export default function Footer() {
     return (
-        <div>
-            <a>twit</a>
-            <a>ig</a>
-            <a>github</a>
-            <a>mirror</a>
+        <div className='footer'>
+            <a className="social-link" href=""><FontAwesomeIcon className="social-links__icon" icon={brands('twitter')} /></a>
+            <a className="social-link" href=""><FontAwesomeIcon className="social-links__icon" icon={brands('instagram')} /></a>
+            <a className="social-link" href=""><FontAwesomeIcon className="social-links__icon" icon={brands('github')} /></a>
+            <a className="social-link" href=""><FontAwesomeIcon className="social-links__icon" icon={brands('dev')} /></a>
         </div>
     )
 }
