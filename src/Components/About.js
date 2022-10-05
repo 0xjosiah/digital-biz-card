a5445ea15e83ec9cc6a32ab109813cf177c1051a
Josiah Webb <jj.webb520@gmail.com>
2022-10-04 19:09:57 -0600

    changes to website and link, changes bio

--- src/Components/About.js
+++ src/Components/About.js
@@ -2,7 +2,7 @@ export default function About() {
     return (
         <div className="info">
             <h2 className="info-header">About</h2>
-        <p className="info-text">I am a self-taught frontend developer with a passion for good design. I enjoy the rollercoaster of problem solving and always enjoy learning new things. I have a particular interest in decentralization and blockchain technologies.</p>
+        <p className="info-text">I am a self-taught frontend developer with a passion for good design. I enjoy the rollercoaster of problem solving and learning new things. I have a particular interest in decentralization and blockchain technologies.</p>
         </div>
     )
 }
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -6,7 +6,7 @@ export default () => (
         <div className="headshot"></div>
         <h1 className="name">Josiah Webb</h1>
         <h3 className="title">Frontend Developer</h3>
-        <a href="https://www.josiahwebb.com" target="_blank" className="website">josiahwebb.com</a>
+        <a href="https://0xjosiah.xyz" target="_blank" className="website">0xjosiah.xyz</a>
         <div className="contact-links">
             <a className="contact-link__email" href="mailto:hello@josiahwebb.com" target="_blank"><FontAwesomeIcon className="contact-links__icon" icon={solid('envelope')} />Email</a>
             <a className="contact-link__linked" href="https://www.linkedin.com/in/josiahwebb/" target="_blank"><FontAwesomeIcon className="contact-links__icon" icon={brands('linkedin')} />LinkedIn</a>
